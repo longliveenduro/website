5817bd0f34792d23133e866a5ffa3259eceab776
Chris Wewerka <chris.wewerka@gmail.com>
2026-02-08 21:26:46 +0100

    added chris website crawl

---
+++ chris-wewerka.de/index7b17.php
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! 1.5 - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">Welcome to Three Dimensions - Chris Wewerka Software Development</title>
+	<subtitle type="text">ThreeDimensions. Scala, Java, JEE and Spring expert Know how</subtitle>
+	<link rel="alternate" type="text/html" href="http://chris-wewerka.de"/>
+	<id>http://chris-wewerka.de/index.php</id>
+	<updated>2026-02-08T19:57:01Z</updated>
+	<generator uri="http://joomla.org" version="1.5">Joomla! 1.5 - Open Source Content Management</generator>
+<link rel="self" type="application/atom+xml" href="http://chris-wewerka.de/index.php?format=feed&amp;type=atom" />
+	<entry>
+		<title>Welcome to Chris Wewerka Software Development</title>
+		<link rel="alternate" type="text/html" href="http://chris-wewerka.de/component/content/article/2-description/2-welcome-to-chris-wewerka-software-development"/>
+		<published>2010-01-07T14:55:14Z</published>
+		<updated>2010-01-07T14:55:14Z</updated>
+		<id>http://chris-wewerka.de/component/content/article/2-description/2-welcome-to-chris-wewerka-software-development</id>
+		<author>
+			<name>Administrator</name>
+		<email>chris@threedimensions.de</email>
+		</author>
+		<summary type="html">&lt;p&gt;I`m looking forward to support you in your &lt;strong&gt;Scala&lt;/strong&gt;, &lt;strong&gt;Kotlin and Java &amp;amp; Python, &lt;/strong&gt;&lt;strong&gt;Microservice&lt;/strong&gt;, &lt;strong&gt;ZIO (Programming with functional effects in general), Big Data + Machine Learning&lt;/strong&gt;, &lt;strong&gt;Kafka&lt;/strong&gt; and Spring projects. I have over 23 years of experience in &lt;strong&gt;Software architecture&lt;/strong&gt;, &lt;strong&gt;Software Development&lt;/strong&gt;, &lt;strong&gt;DevOps&lt;/strong&gt; and &lt;strong&gt;Coaching&lt;/strong&gt; ranging from front end technologies to back end implementation. Also I have more than 10 years of real project experience in Scala and building Microservices with reactive Frameworks in the cloud. Since one year I program with functional effects using &lt;strong&gt;ZIO 1 + 2&lt;/strong&gt; with Scala 3. Several years of experience with &lt;strong&gt;Big Data&lt;/strong&gt; technologies like &lt;strong&gt;Kafka&lt;/strong&gt; may help you too. Strong experience in &lt;strong&gt;Docker&lt;/strong&gt; guaranteed. I'm also an expert in setting up build systems and continuous integration to optimize the productivity of the whole development team. Over one year I also got a lot of experience in &lt;strong&gt;AI&lt;/strong&gt; and &lt;strong&gt;Prompt Engineering&lt;/strong&gt;.&lt;/p&gt;
+&lt;h2&gt;Typical roles in projects&lt;/h2&gt;
+&lt;ul&gt;
+&lt;li&gt; Software Architect&lt;/li&gt;
+&lt;li&gt;Senior Developer&lt;/li&gt;
+&lt;li&gt;Technology Consultant&lt;/li&gt;
+&lt;li&gt;Coach&lt;/li&gt;
+&lt;li&gt;Prompt Engineer&lt;/li&gt;
+&lt;/ul&gt;
+&lt;h2&gt;Technologies&lt;/h2&gt;
+&lt;p&gt;&lt;strong&gt;Scala&lt;/strong&gt;, Kotlin, &lt;strong&gt;Docker&lt;/strong&gt;, Java (all currently released versions),&lt;strong&gt; Amazon Web Services&lt;/strong&gt;, &lt;strong&gt;Kafka&lt;/strong&gt;, &lt;strong&gt;Microservices&lt;/strong&gt;, &lt;strong&gt;Spark&lt;/strong&gt;, JEE, Spring (including in depth knowledge of several Spring projects like Spring Webflow, Spring MVC, Spring Batch), Android, Groovy &amp;amp; Grails, Hibernate, JMS + AMQP + MQTT, SQL Databases, Application Servers (JBoss, Weblogic, Websphere), Webservices, strong focus on testing: unit and integration testing (JUnit, DBUnit, Mockito, EasyMock, Selenium, Scalatest) and Acceptance testing (Fitnesse), detailed Continuous integration know how (Cruisecontrol, Jenkins, Gitlab) and build system know how (Maven, Ant, SBT).&lt;/p&gt;
+&lt;p&gt;See my &lt;a href=&quot;http://chris-wewerka.de/index.php/profile&quot;&gt;profile &lt;/a&gt;for further details.&lt;/p&gt;
+&lt;h2&gt;Certificates&lt;/h2&gt;
+&lt;ul&gt;
+&lt;li&gt;For Scala:&lt;br /&gt; 
+&lt;ul&gt;
+&lt;li&gt;&lt;a href=&quot;http://chris-wewerka.de/attachments/article/2/scala-coursera-certificate.pdf&quot; target=&quot;_blank&quot;&gt;Coursera functional programming course certificate&lt;/a&gt;&lt;/li&gt;
+&lt;li&gt;&lt;a href=&quot;http://chris-wewerka.de/attachments/article/2/Coursera%20reactive%202014&quot; target=&quot;_blank&quot;&gt;Reactive Programming Certificate&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;/li&gt;
+&lt;li&gt;&lt;a href=&quot;https://www.udemy.com/certificate/UC-c652bed0-0d8c-4dbf-9d7c-615ab890cb94/&quot;&gt;Python for Data Science and Machine Learning Bootcamp&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;</summary>
+		<content type="html">&lt;p&gt;I`m looking forward to support you in your &lt;strong&gt;Scala&lt;/strong&gt;, &lt;strong&gt;Kotlin and Java &amp;amp; Python, &lt;/strong&gt;&lt;strong&gt;Microservice&lt;/strong&gt;, &lt;strong&gt;ZIO (Programming with functional effects in general), Big Data + Machine Learning&lt;/strong&gt;, &lt;strong&gt;Kafka&lt;/strong&gt; and Spring projects. I have over 23 years of experience in &lt;strong&gt;Software architecture&lt;/strong&gt;, &lt;strong&gt;Software Development&lt;/strong&gt;, &lt;strong&gt;DevOps&lt;/strong&gt; and &lt;strong&gt;Coaching&lt;/strong&gt; ranging from front end technologies to back end implementation. Also I have more than 10 years of real project experience in Scala and building Microservices with reactive Frameworks in the cloud. Since one year I program with functional effects using &lt;strong&gt;ZIO 1 + 2&lt;/strong&gt; with Scala 3. Several years of experience with &lt;strong&gt;Big Data&lt;/strong&gt; technologies like &lt;strong&gt;Kafka&lt;/strong&gt; may help you too. Strong experience in &lt;strong&gt;Docker&lt;/strong&gt; guaranteed. I'm also an expert in setting up build systems and continuous integration to optimize the productivity of the whole development team. Over one year I also got a lot of experience in &lt;strong&gt;AI&lt;/strong&gt; and &lt;strong&gt;Prompt Engineering&lt;/strong&gt;.&lt;/p&gt;
+&lt;h2&gt;Typical roles in projects&lt;/h2&gt;
+&lt;ul&gt;
+&lt;li&gt; Software Architect&lt;/li&gt;
+&lt;li&gt;Senior Developer&lt;/li&gt;
+&lt;li&gt;Technology Consultant&lt;/li&gt;
+&lt;li&gt;Coach&lt;/li&gt;
+&lt;li&gt;Prompt Engineer&lt;/li&gt;
+&lt;/ul&gt;
+&lt;h2&gt;Technologies&lt;/h2&gt;
+&lt;p&gt;&lt;strong&gt;Scala&lt;/strong&gt;, Kotlin, &lt;strong&gt;Docker&lt;/strong&gt;, Java (all currently released versions),&lt;strong&gt; Amazon Web Services&lt;/strong&gt;, &lt;strong&gt;Kafka&lt;/strong&gt;, &lt;strong&gt;Microservices&lt;/strong&gt;, &lt;strong&gt;Spark&lt;/strong&gt;, JEE, Spring (including in depth knowledge of several Spring projects like Spring Webflow, Spring MVC, Spring Batch), Android, Groovy &amp;amp; Grails, Hibernate, JMS + AMQP + MQTT, SQL Databases, Application Servers (JBoss, Weblogic, Websphere), Webservices, strong focus on testing: unit and integration testing (JUnit, DBUnit, Mockito, EasyMock, Selenium, Scalatest) and Acceptance testing (Fitnesse), detailed Continuous integration know how (Cruisecontrol, Jenkins, Gitlab) and build system know how (Maven, Ant, SBT).&lt;/p&gt;
+&lt;p&gt;See my &lt;a href=&quot;http://chris-wewerka.de/index.php/profile&quot;&gt;profile &lt;/a&gt;for further details.&lt;/p&gt;
+&lt;h2&gt;Certificates&lt;/h2&gt;
+&lt;ul&gt;
+&lt;li&gt;For Scala:&lt;br /&gt; 
+&lt;ul&gt;
+&lt;li&gt;&lt;a href=&quot;http://chris-wewerka.de/attachments/article/2/scala-coursera-certificate.pdf&quot; target=&quot;_blank&quot;&gt;Coursera functional programming course certificate&lt;/a&gt;&lt;/li&gt;
+&lt;li&gt;&lt;a href=&quot;http://chris-wewerka.de/attachments/article/2/Coursera%20reactive%202014&quot; target=&quot;_blank&quot;&gt;Reactive Programming Certificate&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;/li&gt;
+&lt;li&gt;&lt;a href=&quot;https://www.udemy.com/certificate/UC-c652bed0-0d8c-4dbf-9d7c-615ab890cb94/&quot;&gt;Python for Data Science and Machine Learning Bootcamp&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;</content>
+	</entry>
+</feed>
---
+++ chris-wewerka.de/indexc0d0.php
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! 1.5 - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Welcome to Three Dimensions - Chris Wewerka Software Development</title>
+		<description>ThreeDimensions. Scala, Java, JEE and Spring expert Know how</description>
+		<link>http://chris-wewerka.de/component/content/frontpage</link>
+		<lastBuildDate>Sun, 08 Feb 2026 19:57:00 +0000</lastBuildDate>
+		<generator>Joomla! 1.5 - Open Source Content Management</generator>
+		<language>en-gb</language>
+		<item>
+			<title>Welcome to Chris Wewerka Software Development</title>
+			<link>http://chris-wewerka.de/component/content/article/2-description/2-welcome-to-chris-wewerka-software-development</link>
+			<guid>http://chris-wewerka.de/component/content/article/2-description/2-welcome-to-chris-wewerka-software-development</guid>
+			<description><![CDATA[<p>I`m looking forward to support you in your <strong>Scala</strong>, <strong>Kotlin and Java &amp; Python, </strong><strong>Microservice</strong>, <strong>ZIO (Programming with functional effects in general), Big Data + Machine Learning</strong>, <strong>Kafka</strong> and Spring projects. I have over 23 years of experience in <strong>Software architecture</strong>, <strong>Software Development</strong>, <strong>DevOps</strong> and <strong>Coaching</strong> ranging from front end technologies to back end implementation. Also I have more than 10 years of real project experience in Scala and building Microservices with reactive Frameworks in the cloud. Since one year I program with functional effects using <strong>ZIO 1 + 2</strong> with Scala 3. Several years of experience with <strong>Big Data</strong> technologies like <strong>Kafka</strong> may help you too. Strong experience in <strong>Docker</strong> guaranteed. I'm also an expert in setting up build systems and continuous integration to optimize the productivity of the whole development team. Over one year I also got a lot of experience in <strong>AI</strong> and <strong>Prompt Engineering</strong>.</p>
+<h2>Typical roles in projects</h2>
+<ul>
+<li> Software Architect</li>
+<li>Senior Developer</li>
+<li>Technology Consultant</li>
+<li>Coach</li>
+<li>Prompt Engineer</li>
+</ul>
+<h2>Technologies</h2>
+<p><strong>Scala</strong>, Kotlin, <strong>Docker</strong>, Java (all currently released versions),<strong> Amazon Web Services</strong>, <strong>Kafka</strong>, <strong>Microservices</strong>, <strong>Spark</strong>, JEE, Spring (including in depth knowledge of several Spring projects like Spring Webflow, Spring MVC, Spring Batch), Android, Groovy &amp; Grails, Hibernate, JMS + AMQP + MQTT, SQL Databases, Application Servers (JBoss, Weblogic, Websphere), Webservices, strong focus on testing: unit and integration testing (JUnit, DBUnit, Mockito, EasyMock, Selenium, Scalatest) and Acceptance testing (Fitnesse), detailed Continuous integration know how (Cruisecontrol, Jenkins, Gitlab) and build system know how (Maven, Ant, SBT).</p>
+<p>See my <a href="http://chris-wewerka.de/index.php/profile">profile </a>for further details.</p>
+<h2>Certificates</h2>
+<ul>
+<li>For Scala:<br /> 
+<ul>
+<li><a href="http://chris-wewerka.de/attachments/article/2/scala-coursera-certificate.pdf" target="_blank">Coursera functional programming course certificate</a></li>
+<li><a href="http://chris-wewerka.de/attachments/article/2/Coursera%20reactive%202014" target="_blank">Reactive Programming Certificate</a></li>
+</ul>
+</li>
+<li><a href="https://www.udemy.com/certificate/UC-c652bed0-0d8c-4dbf-9d7c-615ab890cb94/">Python for Data Science and Machine Learning Bootcamp</a></li>
+</ul>]]></description>
+			<author>chris@threedimensions.de (Administrator)</author>
+			<category>frontpage</category>
+			<pubDate>Thu, 07 Jan 2010 14:55:14 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
